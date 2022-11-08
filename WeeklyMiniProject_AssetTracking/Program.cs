f1d2cf6bfae6034a77836d195f6b1b87e937d319
patriklussi <patrik.lussi@gmail.com>
2022-11-08 11:12:00 +0100

    added stuff

--- WeeklyMiniProject_AssetTracking/Program.cs
+++ WeeklyMiniProject_AssetTracking/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to your asset tracker");
+
+
+
+while (true)
+{
+    Console.Write("Category: ");
+    string data = Console.ReadLine();
+    if(data == "exit")
+    {
+        break;
+    }
+}
+
+class AssetList
+{
+    List<> assetList = new List<> ();
+}
+
+
+
+
+
+
+class Asset 
+{
+    public Asset()
+    {
+    }
+
+    public Asset(string type, string brand, string model, string office, DateTime purchaseDate, int price, string currency, double localPrice)
+    {
+        Type = type;
+        Brand = brand;
+        Model = model;
+        Office = office;
+        PurchaseDate = purchaseDate;
+        Price = price;
+        Currency = currency;
+        LocalPrice = localPrice;
+    }
+
+    string Type { get; set; }
+    string Brand { get; set; }
+    string Model { get; set; }
+    string Office { get; set; }
+    DateTime PurchaseDate { get; set; }
+    int Price { get; set; }
+    string Currency { get; set; }
+    double LocalPrice { get; set; }
+}
