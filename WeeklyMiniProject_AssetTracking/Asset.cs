3f2e219212ceb235ac37f7eb14400ec5ec4bef3c
patriklussi <patrik.lussi@gmail.com>
2022-11-11 13:42:15 +0100

    Added extra class file and other stuff

---
+++ WeeklyMiniProject_AssetTracking/Asset.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeklyMiniProject_AssetTracking
+{
+    internal class Asset
+    {
+        public string AssetType { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+      public  DateTime ExpirationDate { get; set; }
+    }
+}
--- WeeklyMiniProject_AssetTracking/Program.cs
+++ WeeklyMiniProject_AssetTracking/Program.cs
@@ -1,60 +1,125 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using WeeklyMiniProject_AssetTracking;
+
 Console.WriteLine("Welcome to your asset tracker");
 
 
 
 AssetList assetList = new AssetList();
 
+Console.WriteLine("Enter the type of asset you want to track | You can only track laptops, phones and pcs.");
+
 while (true)
 {
-    Console.Write("Category: ");
-    string data = Console.ReadLine();
-    
-
-
-    if(data == "exit")
+    Console.Write("Type: ");
+    string type = Console.ReadLine();
+    if (type == "exit")
     {
         break;
     }
-    assetList.AddToAssetList(data);
-   
+
+
+    Console.Write("Brand: ");
+    string brand = Console.ReadLine();
+    if (brand == "exit")
+    {
+        break;
+    }
+
+    Console.Write("Model : ");
+    string model = Console.ReadLine();
+    if(model == "exit")
+    {
+        break;
+    }
+    DateTime purchaseDate;
+    Console.WriteLine("Enter date of purchase in format MM/DD/YYYY: ");
+    purchaseDate = DateTime.Parse(Console.ReadLine());
+
+
+
+    assetList.AddToAssetList(type.Trim(),brand.Trim(),purchaseDate,model.Trim());
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("You have succesfully added your asset to the list");
+    Console.ResetColor();
 }
+assetList.DisplayList();
+Console.WriteLine("Exited");
 Console.ReadLine();
+
+
 
 class AssetList
 {
-    List<Asset> assetList = new List<Asset> ();
-    public void AddToAssetList(string data)
+    List<Asset> assetList = new List<Asset>();
+
+    
+    public void AddToAssetList(string data ,string brand,DateTime purchaseDate,string model)
     {
-        assetList.Add(new Laptop(data));
+        if (data == "Phone") 
+        {
+            assetList.Add(new Phone(data,brand));
+        }
+        else if (data == "Laptop")
+        {
+            assetList.Add(new Laptop(data,brand,purchaseDate,model));
+        }
+        else if (data == "Stationary computer")
+        {
+            assetList.Add(new StationaryComputer(data,brand));
+        }
+
+    }
+
+
+    public void DisplayList()
+    {
+        Console.WriteLine("Hello i do stuff");
+        foreach (Asset asset in assetList)
+        {
+            Console.WriteLine("Asset category " + asset.AssetType + " " +  asset.Brand + " " + asset.Model + " "+ asset.ExpirationDate.ToString());
+        }
     }
 }
 
-class Asset 
-{
-    public Asset()
-    {
-    }
 
-    
-    
-}
 
 class Laptop : Asset
 {
-    public Laptop(string type)
+    public Laptop()
     {
-        Type = type;
     }
 
-    public string Type {get;set;}
+  
+    public Laptop(string assetType,string brand,DateTime purchaseDate,string model)
+    {
+        AssetType = assetType;
+        Brand = brand;
+        ExpirationDate = purchaseDate;
+        Model = model;
+    }
+
+   
 }
-class Phone :Asset
-{
 
+class Phone : Asset
+{
+    public Phone(string typeOfAsset, string brand)
+    {
+        AssetType = typeOfAsset;
+        Brand = brand;
+    }
+
+  
 }
 
-class StationaryComputer: Asset
+class StationaryComputer : Asset
 {
+    public StationaryComputer(string typeOfAsset, string brand)
+    {
+        AssetType = typeOfAsset;
+        Brand = brand;
+    }
 
+   
 }
